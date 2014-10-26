d15a8ad0453cad075f8e3ca8dbe5ca7b5c8962f9
acrost <dirkwhitaker@gmail.com>
2014-10-26 16:52:50 -0400

    "edited comment formatting"

--- cachematrix.R
+++ cachematrix.R
@@ -8,19 +8,19 @@ makeCacheMatrix <- function(x = matrix()) { ## this function creates a cache mat
 		i <- NULL				## initial inverse matrix value set to NULL
 		
 		set <- function(y){		##	this function will set 'x' to the argument 
-			x <<- y				##							value (a matrix)
+			x <<- y				##	value (a matrix)
 								## 
 			
 			i <<- NULL			## inverse matrix 'i' is reset to NULL because 
-		}						## 		the inverse of the new matrix 
-								## 		hasn't been computed yet
+		}						## 	the inverse of the new matrix 
+								## 	hasn't been computed yet
 								
 		get<- function(){  		## this function will return the matrix 'x'
 			x
 		}
 		setinverse <-function(solve){	## this function replaces the value of 'i' 
-			i <<- solve					##  	in the global environment with the  
-										##		newly computed inverse matrix 
+			i <<- solve					##  in the global environment with the  
+										##	newly computed inverse matrix 
 		}
 		getinverse <-function(){		## this function returns the inverse matrix 'i'
 			i
@@ -28,7 +28,7 @@ makeCacheMatrix <- function(x = matrix()) { ## this function creates a cache mat
 		
 		list(set = set, get = get, setinverse = setinverse, 
 				getinverse = getinverse)	## returns a list of the functions defined
-											##  in the parent function makeCacheMatrix
+											## in the parent function makeCacheMatrix
 													
 
 }
@@ -41,12 +41,12 @@ cacheSolve <- function(x, ...) {				 ## Return a matrix that is the inverse of '
 		i <-x$getinverse()			## loads the newest value of the inverse matrix 
 		if(!is.null(i)){			##  if the inverse matrix was already computed
 			message("getting cached data") ## then tell user that cached value 
-									## 		of the inverse matrix will be used  
+									## 	of the inverse matrix will be used  
 			return (i)   			##	return the cached value of the inverse matrix 'i'
 		}
 		data<-x$get()			## load the matrix data
 		i<-solve(data, ...)		## use the solve() function to obtain the inverse matrix
 		x$setinverse(i) 		## updates i to new inverse matrix, assigns it as the 
-								## 				inverse matrix for 'x'
+								## inverse matrix for 'x'
 		i						## return newly computed inverse matrix 'i' 
 }
